3ddef08033a8b44ef3d02d28e61aa3d3702f77d1
Anisha Jain <anishajain@outlook.com>
2018-10-02 14:01:41 -0700

    cookies work

--- scripts.js
+++ scripts.js
@@ -53,7 +53,7 @@ buyClickPower.addEventListener("click", function() {
         clickPowerPriceAmount = Math.floor(clickPowerPriceAmount * 1.33);
     
         //update Click Power
-        clickPower += 1;
+        clickPower += 1 * Math.floor(clickPowerLevelNumber * 1.05);
     
         //refresh shop item
         refreshPowerClick();
@@ -70,12 +70,7 @@ let refreshPowerClick = function() {
 }
     
 
-let autoGrandmaStart = function() {
-    let grandmaInt = window.setInterval(function(){
-      cookieCount += grandmaPower;
-      refreshCookieCount();
-    }, 1000);
-  }
+
 
 
 /********************************
@@ -103,6 +98,13 @@ let refreshGrandma = function() {
     grandmaMultiple.innerHTML = grandmaPower - 10;
   }
 
+  let autoGrandmaStart = function() {
+    let grandmaInt = window.setInterval(function(){
+      cookieCount += grandmaPower;
+      refreshCookieCount();
+    }, 1000);
+  }
+
 //buy a grandma
 buyGrandma.addEventListener("click", function() {
 
@@ -111,24 +113,24 @@ buyGrandma.addEventListener("click", function() {
         // Subtract cookies from the price of the item.
         cookieCount +=  - grandmaPriceAmount;
         refreshCookieCount()
-  
-    //upgrade power level
-    grandmaLevelNumber += 1;
-  
-    //update price
-    grandmaPriceAmount = Math.floor(grandmaPriceAmount * 1.33);
-  
-    //update grandma power
-    grandmaPower += 10;
-  
-    //turn autoGrandma on!
-    autoGrandma = true
-    autoGrandmaStart();
-  
-    //refresh shop item
-    refreshGrandma();
-  
-  }
+    
+        //upgrade power level
+        grandmaLevelNumber += 1;
+    
+        //update price
+        grandmaPriceAmount = Math.floor(grandmaPriceAmount * 1.33);
+    
+        //update grandma power
+        grandmaPower += 10 + Math.floor(grandmaLevelNumber * 1.33);
+    
+        //turn autoGrandma on!
+        autoGrandma = true
+        autoGrandmaStart();
+    
+        //refresh shop item
+        refreshGrandma();
+    
+    }
 });
 
 /********************************
@@ -172,25 +174,192 @@ buyFacility.addEventListener("click", function() {
         cookieCount -= facilityPriceAmount;
         refreshCookieCount()
 
-    //upgrade power level
-    facilityLevelNumber += 1;
+        //upgrade power level
+        facilityLevelNumber += 1;
 
-    //update price
-    facilityPriceAmount = Math.floor(facilityPriceAmount * 1.33);
+        //update price
+        facilityPriceAmount = Math.floor(facilityPriceAmount * 1.33);
 
-    //update facility power
-    facilityPower += 600;
+        //update facility power
+        facilityPower += 600 + Math.floor(facilityLevelNumber * 1.33);;
 
-    //turn autoFacility on!
-    facilityAuto = true
-    autoFacilityStart();
+        //turn autoFacility on!
+        facilityAuto = true
+        autoFacilityStart();
 
-    //refresh shop item
-    refreshFacility();
+        //refresh shop item
+        refreshFacility();
 
   }
 })
 
-//game loop
 
-//refresh shop
+/********************************
+
+          Doubler
+
+********************************/
+
+//set default variables
+let doublerPriceAmount = 1999;
+let doublerLevelNumber = 0;
+//let doublerAmount = cookieCount;
+
+//declare DOM variables
+let buyDoubler = document.getElementById('buy-doubler');
+let doublerPrice = document.getElementById('doubler-price');
+let doublerLevel = document.getElementById('doubler-level');
+
+let refreshDoubler = function() {
+    doublerPrice.innerHTML = doublerPriceAmount;
+    doublerLevel.innerHTML = doublerLevelNumber;
+}
+
+
+buyDoubler.addEventListener('click', () => {
+    // upgrade doubler level
+    doublerLevelNumber += 1;
+
+    if (cookieCount >= doublerPriceAmount) {
+        // update cookies
+        cookieCount -= doublerPriceAmount;
+        refreshCookieCount();
+    
+        // update price
+        doublerPriceAmount = Math.floor(doublerPriceAmount* 1.33);
+
+        // double cookies
+        cookieCount = (cookieCount) * 2;
+        refreshCookieCount();
+
+        refreshDoubler();
+    }
+
+})
+
+
+
+
+
+/********************************
+
+        Zombie Cookie Maker
+
+********************************/
+
+
+//set default variables
+let zombiePriceAmount = 2999;
+let zombieLevelNumber = 0;
+let zombiePower = 100; 
+let zombieAdd = 3000;
+let zombieMin = 0;
+
+//declare DOM variables
+let buyZombie = document.getElementById('buy-zombie');
+let zombiePrice = document.getElementById('zombie-price');
+let zombieLevel = document.getElementById('zombie-level');
+
+
+let refreshZombie = function() {
+    zombiePrice.innerHTML = zombiePriceAmount;
+    zombieLevel.innerHTML = zombieLevelNumber;
+}
+
+let autoZombieStart = function() {
+    let facilityInt = window.setInterval(function(){
+        let num = Math.random(zombieMin, zombieAdd);
+        let diff = Math.random(100);
+        cookieCount += Math.floor(num - diff);
+        refreshCookieCount();
+    }, 1000);
+}
+
+buyZombie.addEventListener('click', () => {
+    // upgrade doubler level
+    zombieLevelNumber += 1;
+
+    if (cookieCount >= zombiePriceAmount) {
+        // update cookies
+        cookieCount -= zombiePriceAmount;
+        refreshCookieCount();
+        // update price
+        zombiePriceAmount = Math.floor(zombiePriceAmount* 1.33);
+
+        autoZombieStart();
+        zombieAdd += 1000;
+        zombieMin += 50;
+        refreshZombie();
+    }
+
+    
+
+})
+
+/********************************
+
+        BUNBUN COOKIES
+
+********************************/
+
+//set default values
+let bunBunAuto = false;
+let bunBunPower = 10000;
+let bunBunPriceAmount = 9999999;
+let bunBunLevelNumber = 0;
+
+//declare DOM variables
+let buyBunBun = document.getElementById('buy-bunbun');
+let bunBunPrice = document.getElementById('bunbun-price');
+let bunBunLevel = document.getElementById('bunbun-level');
+let bunBunMultiple = document.getElementById('bunbun-multiple');
+
+let refreshBunBun = function() {
+    bunBunLevel.innerHTML = bunBunLevelNumber
+    bunBunPrice.innerHTML = bunBunPriceAmount;
+    bunBunMultiple.innerHTML = bunBunPower;
+    bunBunMultiple.innerHTML = bunBunPower - 10;
+  }
+
+let autoBunBunStart = function() {
+let bunBunInt = window.setInterval(function(){
+    cookieCount += bunBunPower;
+    refreshCookieCount();
+}, 1000);
+}
+
+//buy a bunny
+buyBunBun.addEventListener("click", function() {
+
+    //make sure we have enough cookies and subtract our cookies from the price
+    if (cookieCount >= bunBunPriceAmount) {
+        // Subtract cookies from the price of the item.
+        cookieCount +=  - bunBunPriceAmount;
+        refreshCookieCount()
+  
+        //upgrade power level
+        bunBunLevelNumber += 1;
+    
+        //update price
+        bunBunPriceAmount = Math.floor(bunBunPriceAmount * 1.33);
+        
+        //turn bunny on!
+        bunBunAuto = true
+        autoBunBunStart();
+    
+        //refresh shop item
+        refreshBunBun();
+
+        //update bunny power
+        bunBunPower += Math.floor(bunBunPowert* 1.33);;
+  
+  }
+});
+
+
+// draw cookies
+let maxY = window.innerHeight;
+let maxX = window.innerWidth;
+
+console.log(maxY);
+console.log(maxX);
